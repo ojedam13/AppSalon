03497e624609340880546b51d10fa97acac10934
ojedam13 <m_ojeda17@hotmail.com>
2021-11-06 12:33:36 -0300

    creando html de los servicios

--- src/js/app.js
+++ src/js/app.js
@@ -19,6 +19,27 @@ async function mostrarServicios() {
             const { id, nombre, precio } = servicio;
             
             //Dom scripting
+            //generar nombre servicio
+            const nombreServicio = document.createElement('P');
+            nombreServicio.textContent = nombre;
+            nombreServicio.classList.add('nombre-servicio');
+
+            //generar precio
+            const precioServicio = document.createElement('P');
+            precioServicio.textContent = `$ ${precio}`;
+            precioServicio.classList.add('precio-servicio');
+
+            //generar div contendedor servicio
+            const servicioDiv = document.createElement('DIV');
+            servicioDiv.classList.add('servicio');
+
+            //Inyectar precio y nombre al div de servicio
+            servicioDiv.appendChild(nombreServicio);
+            servicioDiv.appendChild(precioServicio);
+
+            // iyectar en el html
+            document.querySelector('#servicios').appendChild(servicioDiv);
+
         });
     } catch (error) {
         
